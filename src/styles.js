716c5cc105454433d072caf5fb73e34a4352197c
Emily Zhang <emily.fangdi.zhang@hotmail.com>
2023-12-20 18:00:31 -0800

    added global text fonts

--- src/styles.js
+++ src/styles.js
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 
 export const mobile = `@media (max-width: 768px)`;
 
+export const textFont = 'Cormorant Unicase';
+export const decorativeFont = 'Unifraktur Maguntia';
+
 export const Bold = styled.span`
   font-weight: 700;
 `;
